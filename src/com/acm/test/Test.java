0e4dfbd78babd054d2e4526898ba078c02a4f14a
Alun <3108889804@qq.com>
2023-08-05 11:25:22 +0800

    2023/8/5

--- src/com/acm/test/Test.java
+++ src/com/acm/test/Test.java
@@ -7,9 +7,38 @@ public class Test {
 //            System.out.println(""+i+":   " + (i >>  1));
 //        }
 
-        int x = 4;
-        for (int i=0;i<10;i++) {
-            System.out.println(x^i ^ i);
+//        int x = 4;
+//        for (int i=0;i<10;i++) {
+//            System.out.println(x^i ^ i);
+//        }
+
+        int x = 1;
+        int y = 1;
+        if(x++==2&++y==2){
+            x=10;
         }
+        System.out.println("二、x为"+x+"，y为"+y+"。");
+
+        new Test().test();
     }
+
+    public void test() {
+        init();
+        load();
+        test2("test");
+    }
+
+    private void test2(String test) {
+        System.out.println(test);
+    }
+
+    private void load() {
+        System.out.println("load");
+    }
+
+    private void init() {
+        System.out.println("init");
+    }
+
+
 }
