9f2774be9ec2383a8b13e86021ec9e8d9b2a49e7
rain <3108889804@qq.com>
2024-03-24 00:05:14 +0800

    add

--- src/com/acm/newcode/huaweijishi/HJ11.java
+++ src/com/acm/newcode/huaweijishi/HJ11.java
@@ -11,9 +11,7 @@ public class HJ11 {
                 System.out.println(0);
                 continue;
             }
-//            System.out.println();
-//            System.out.println(Integer.MAX_VALUE);
-//            System.out.println(Integer.MIN_VALUE);
+
             StringBuilder sb = new StringBuilder();
             while (num != 0) {
                 int t = num % 10;
